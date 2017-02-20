20268286ba9a9016174220de78a476541cef57ac
Roman Gullaksen <rgullaksen@gmail.com>
2017-02-20 17:25:22 +0100

    added 2 funktions.

---
+++ NextGen/NextGen/Payment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextGen
+{
+    internal class Payment
+    {
+        private double amount;
+
+        public Payment(double cashtendered)
+        {
+            amount = cashtendered;
+        }
+
+        public double getAmount()
+        {
+            return amount;
+        }
+    }
+}
